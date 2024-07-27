cf9051efa810204b323f3857152f2787d2421eb1
uday-chowdary018 <udaychowdhary743@gmail.com>
2024-07-27 16:45:38 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
+// hooks nothing but the function (predefine funtion in react)
+//hooks are named export funtion . while import we have desturture
+// in react hooks are useding within the component. if we use in function, class, in return block, inline block it give the error
+// we can also create custom(userdefine) hooks 
+// we can't use hooks in condition statements
+//hooks useed in components at top level
 function App() {
+
+  useState();
   return (
+    //useState() its give error 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>hello world</h1>
       </header>
     </div>
   );
